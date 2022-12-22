721066f249bc28aeed16257029e4e1612334ce9e
mrzhangfelix <805908873@qq.com>
2022-12-22 15:04:29 +0800

    com

--- yys/tansuo.py
+++ yys/tansuo.py
@@ -10,35 +10,33 @@ def main():
 	a=1
 	b=1
 	while True:
-		if button("攻击.png"):
+		if button("攻击.png",0.7):
 			print("小怪次数:"+str(b))
 			b=b+1
-			time.sleep(2)
+			time.sleep(5)
 			continue
-		if button("攻击首领.png"):
+		if button("攻击首领.png",0.7):
 			print("首领次数:"+str(a))
 			a=a+1
+			time.sleep(5)
+			continue
+		if button("捡东西.png",0.7):
+			continue
+		if button("探索.png",0.7):
 			time.sleep(2)
 			continue
-		if button("捡东西.png"):
-			time.sleep(2)
-			continue
-		if button("探索.png"):
-			time.sleep(2)
-			continue
-		if button("二十八章.png"):
-			time.sleep(2)
-			continue
+		# X: 2196 , Y:  809
+		pyautogui.moveTo(2196,809)
 		pyautogui.click()
 		time.sleep(1)
 
 
-def button(png):
+def button(png,confidence):
 	# 事先对按钮截图
 	Img = Image.open(png)
 	time.sleep(1)
 	# 截图当前屏幕并找到之前加载的按钮截图 confidence的值决定精度
-	msg = pyautogui.locateOnScreen(Img, confidence=0.7, grayscale=True)
+	msg = pyautogui.locateOnScreen(Img, confidence=confidence, grayscale=True)
 	if msg == None:
 		return False
 	else:
