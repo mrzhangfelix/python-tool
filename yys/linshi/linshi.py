e8755ad6e5ba2b6931b6c2377b2df339af840de0
mrzhangfelix <805908873@qq.com>
2022-12-30 09:59:01 +0800

    com

--- yys/linshi/linshi.py
+++ yys/linshi/linshi.py
@@ -11,12 +11,14 @@ def main():
     # 打开窗口在固定位置
     get_window_pos("阴阳师 - MuMu模拟器",pos)
 
+    # attackImg = Image.open("挑战.png")
     attackImg = Image.open("img.png")
     prepareImg = Image.open("prepare.png")
     endImg = Image.open("end.png")
     while True:
         if button(attackImg,pos):
-            print("点击attack按钮")
+            print("点击attack按钮count:{}".format(count))
+            count=count+1
             sleep(180)
         # if button(prepareImg,pos):
         #     print("点击准备按钮")
